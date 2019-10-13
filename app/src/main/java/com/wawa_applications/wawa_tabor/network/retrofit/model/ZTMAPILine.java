5403427817de8776e322c4044e9dacaa8355d54a
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2019-10-13 21:20:17 +0200

    correct serialization of Line model

--- app/src/main/java/com/wawa_applications/wawa_tabor/network/retrofit/model/ZTMAPILine.java
+++ app/src/main/java/com/wawa_applications/wawa_tabor/network/retrofit/model/ZTMAPILine.java
@@ -1,6 +1,7 @@
 package com.wawa_applications.wawa_tabor.network.retrofit.model;
 
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +11,23 @@ import lombok.NoArgsConstructor;
 public class ZTMAPILine {
 
     @Expose
+    @SerializedName("Lat")
     private Double lat;
+
     @Expose
+    @SerializedName("Lon")
     private Double lon;
+
     @Expose
+    @SerializedName("Time")
     private String time;
+
     @Expose
+    @SerializedName("Lines")
     private String line;
+
     @Expose
+    @SerializedName("Brigade")
     private String brigade;
 
     public ZTMAPILine(Double lat, Double lon, String time, String line, String brigade) {
