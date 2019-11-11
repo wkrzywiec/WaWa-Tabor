990686899786e530736af46a017168e8ef9cdffe
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2019-11-11 18:21:13 +0100

    #26 sonar test coverage

--- app/src/main/java/com/wawa_applications/wawa_tabor/exception/BusNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.wawa_applications.wawa_tabor.exception;
-
-public class BusNotFoundException extends RuntimeException {
-
-    public BusNotFoundException(String line){
-        super("Nie ma takiego autobusu o numerze: " + line);
-    }
-}
--- app/src/main/java/com/wawa_applications/wawa_tabor/exception/TramNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.wawa_applications.wawa_tabor.exception;
-
-public class TramNotFoundException extends RuntimeException {
-
-    public TramNotFoundException(String line){
-        super("Nie ma takiego autobusu o numerze: " + line);
-    }
-}
